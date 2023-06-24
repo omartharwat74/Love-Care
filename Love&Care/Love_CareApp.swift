ab60f2098946309933ddfd93d570c4d079529e88
Omar Tharwat <77683787+omartharwat74@users.noreply.github.com>
2023-06-24 17:03:53 +0300

    Initial Commit

---
+++ Love&Care/Love_CareApp.swift
@@ -0,0 +1,17 @@
+//
+//  Love_CareApp.swift
+//  Love&Care
+//
+//  Created by Omar Tharwat on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Love_CareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
